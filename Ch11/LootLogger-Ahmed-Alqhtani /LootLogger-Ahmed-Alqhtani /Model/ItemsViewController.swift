c6ee552b24943452dacef8f9c2cd93123850888e
ahemdalq <91871668+ahemdalq@users.noreply.github.com>
2021-12-05 14:29:25 +0300

    LootLogger Cjh11-Ch12

---
+++ Ch11/LootLogger-Ahmed-Alqhtani /LootLogger-Ahmed-Alqhtani /Model/ItemsViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Ahmed awadh alqhtani on 12/04/1443 AH.
+//
+
+import UIKit
+
+class ItemsViewController:UITableViewController {
+  
+  var itemStore: ItemStore!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 65
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  }
+  @IBAction func addNewItem(_ sender: UIButton) {
+    let newItem = itemStore.createItem()
+    if let index = itemStore.allItems.firstIndex(of: newItem) {
+      let indexPath = IndexPath(row: index, section: 0)
+      tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+  }
+  @IBAction func toggleEditingMode(_ sender: UIButton) {
+    if isEditing {
+      // Change text of button to inform user of state
+      sender.setTitle("Edit", for: .normal)
+      // Turn off editing mode
+      setEditing(false, animated: true)
+    } else {
+      // Change text of button to inform user of state
+      sender.setTitle("Done", for: .normal)
+      // Enter editing mode
+      setEditing(true, animated: true)
+    }
+  }
+  
+  
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return itemStore.allItems.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
+                                             for: indexPath) as! ItemCell
+    let item = itemStore.allItems[indexPath.row]
+    // Configure the cell with the Item
+    cell.nameLabel.text = item.name
+    cell.serialNumberLabel.text = item.serialNumber
+    cell.valueLabel.text = "$\(item.valueInDollars)"
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView,
+                          commit editingStyle: UITableViewCell.EditingStyle,
+                          forRowAt indexPath: IndexPath) {
+    // If the table view is asking to commit a delete command...
+    if editingStyle == .delete {
+      let item = itemStore.allItems[indexPath.row]
+      // Remove the item from the store
+      itemStore.removeItem(item)
+      // Also remove that row from the table view with an animation
+      tableView.deleteRows(at: [indexPath], with: .automatic)
+      
+    }
+    
+  }
+  override func tableView(_ tableView: UITableView,
+                          moveRowAt sourceIndexPath: IndexPath,
+                          // Update the model
+                          to destinationIndexPath: IndexPath) {
+    itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    
+    
+    
+    
+  }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // If the triggered segue is the "showItem" segue
+    switch segue.identifier {
+    case "showItem":
+      // Figure out which row was just tapped
+      if let row = tableView.indexPathForSelectedRow?.row {
+        // Get the item associated with this row and pass it along
+        let item = itemStore.allItems[row]
+        let detailViewController
+          = segue.destination as! DetailViewController
+        detailViewController.item = item
+      } default:
+        preconditionFailure("Unexpected segue identifier.")
+    }
+  }
+  
+}
---
+++ Ch12/LootLogger-Ahmed-Alqhtani /LootLogger-Ahmed-Alqhtani /Model/ ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//   ItemCell.swift
+//  LootLogger
+//
+//  Created by Ahmed awadh alqhtani on 17/04/1443 AH.
+//
+
+import UIKit
+class ItemCell: UITableViewCell {
+  
+  @IBOutlet var nameLabel: UILabel!
+   @IBOutlet var serialNumberLabel: UILabel!
+   @IBOutlet var valueLabel: UILabel!
+
+  
+}
+
